faad3bfec1cb8e95c925f4f3e898c0028a6e77a2
mathphreak <mathphreak@gmail.com>
2011-03-04 19:34:52 -0700

    added support for detecting "you already voted for that"

--- README.markdown
+++ README.markdown
@@ -5,4 +5,4 @@ Visit Canvas Rider at [http://canvasrider.com].
 Features
 ========
 
-* AJAXified (non-synchronous) voting up/down (doesn't distinguish successful voting from "you already voted for that").
+* AJAXified (non-synchronous) voting up/down.
--- work.js
+++ work.js
@@ -3,7 +3,7 @@ var thumbsDown = both.first();
 var thumbsUp = both.last();
 
 var downUrl = thumbsDown.attr("href");
-var tdAttr = "javascript:var h=new XMLHttpRequest();h.open('GET', '" + downUrl + "', true);h.onreadystatechange=function(){if(h.readyState==4&&h.status==200){document.getElementById('social').innerHTML='Voted down.';}};h.send(null);"
+var tdAttr = "javascript:var h=new XMLHttpRequest();h.open('GET', '" + downUrl + "', true);h.onreadystatechange=function(){if(h.readyState==4&&h.status==200){var r=h.responseText;if(r.indexOf('You already voted for that track!')!=-1){document.getElementById('social').innerHTML='You already voted for that track!';}else if(r.indexOf('Your vote has been refused')!=-1){document.getElementById('social').innerHTML='Your vote has been refused.';}else{document.getElementById('social').innerHTML='Voted down.';}}};h.send(null);"
 thumbsDown.attr("href", tdAttr);
 thumbsDown.attr("onClicked", tdAttr);
 var upUrl = thumbsUp.attr("href");
