51d87e49706dd86f75d27e9441a669991016d075
mathphreak <mathphreak@gmail.com>
2011-03-04 18:02:40 -0700

    Added support for tracks/random/ and upgraded to version 0.8.

--- work.js
+++ work.js
@@ -2,10 +2,11 @@ var both = $('h1 a[href^="tracks/"]');
 var thumbsDown = both.first();
 var thumbsUp = both.last();
 
-var currurl = document.URL;
-var tdAttr = "javascript:var h=new XMLHttpRequest();h.open('GET', '" + currurl + "/down', true);h.onreadystatechange=function(){if(h.readyState==4&&h.status==200){document.getElementById('social').innerHTML='Voted down.';}};h.send(null);"
+var downUrl = thumbsDown.attr("href");
+var tdAttr = "javascript:var h=new XMLHttpRequest();h.open('GET', '" + downUrl + "', true);h.onreadystatechange=function(){if(h.readyState==4&&h.status==200){document.getElementById('social').innerHTML='Voted down.';}};h.send(null);"
 thumbsDown.attr("href", tdAttr);
 thumbsDown.attr("onClicked", tdAttr);
-var tuAttr = "javascript:var h=new XMLHttpRequest();h.open('GET', '" + currurl + "/up', true);h.onreadystatechange=function(){if(h.readyState==4&&h.status==200){document.getElementById('social').innerHTML='Voted up.';}};h.send(null);"
+var upUrl = thumbsUp.attr("href");
+var tuAttr = "javascript:var h=new XMLHttpRequest();h.open('GET', '" + upUrl + "', true);h.onreadystatechange=function(){if(h.readyState==4&&h.status==200){document.getElementById('social').innerHTML='Voted up.';}};h.send(null);"
 thumbsUp.attr("href", tuAttr);
 thumbsUp.attr("onClicked", tuAttr);
