3f5a503dac2934d40a0bf6073119268c1b742d0d
mathphreak <mathphreak@gmail.com>
2011-03-05 19:34:24 -0700

    added hosted vcri.js - makes life a lot easier

---
+++ vcri.js
@@ -0,0 +1,44 @@
+// This is the hosted/injected VCRI code.
+
+function vote(target, finishMsg) {
+	var social = document.getElementById('social');
+	social.innerHTML = 'Voting...';
+	var ajax = new XMLHttpRequest();
+	ajax.open('GET', target, true);
+	ajax.onreadystatechange = function() {
+		if (ajax.readyState == 4 && ajax.status == 200) {
+			var response = ajax.responseText;
+			if (response.indexOf('You already voted for that track!') != -1) {
+				s.innerHTML='You already voted for that track!';
+			} else if (response.indexOf('Your vote has been refused') != -1) {
+				s.innerHTML='Your vote has been refused.';
+			} else {
+				s.innerHTML=finishMsg;
+			}
+		}
+	};
+	ajax.send(null);
+}
+
+function randomPositive() {
+	var ajax = new XMLHttpRequest();
+	ajax.open('GET', 'http://canvasrider.com/tracks/random', true);
+	ajax.onreadystatechange = function() {
+		if (ajax.readyState == 4 && ajax.status == 200) {
+			var response = ajax.responseText;
+			if (response.indexOf('class="green"') != -1) { // assume the only green thing is the voting score if it's green
+				var i = response.indexOf('canvas_ride(') + 'canvas_ride('.length;
+				var j = response.indexOf(')', i);
+				var s = response.substring(i, j);
+				window.location.assign('http://canvasrider.com/tracks/' + s);
+			} else {
+				randomPositive(); // try again if it's red
+			}
+		}
+	};
+	ajax.send(null);
+};
+function showRandomMenu() {
+	var m = document.getElementById('menu');
+	m.innerHTML += "<br /><a href='javascript:randomPositive();'>Random positive track</a> / <a href='http://canvasrider.com/tracks/random'>Any random track</a>";
+}
