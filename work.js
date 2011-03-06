6e7142086db2c3c18a499030a8aa514dedfc9800
mathphreak <mathphreak@gmail.com>
2011-03-05 19:58:49 -0700

    expanded to all of canvasrider.com and added update ability, and relocated the hard work to a server-side .js file

--- work.js
+++ work.js
@@ -1,12 +1,20 @@
-var both = $('h1 a[href^="tracks/"]');
-var thumbsDown = both.first();
-var thumbsUp = both.last();
+var headID = document.getElementsByTagName("head")[0];         
+var newScript = document.createElement('script');
+newScript.type = 'text/javascript';
+newScript.src = 'http://mathphreak.github.com/VCRI/vcri.js?crap=' + Math.random();
+headID.appendChild(newScript);
 
-var downUrl = thumbsDown.attr("href");
-var tdAttr = "javascript:var h=new XMLHttpRequest();h.open('GET', '" + downUrl + "', true);h.onreadystatechange=function(){if(h.readyState==4&&h.status==200){var r=h.responseText;if(r.indexOf('You already voted for that track!')!=-1){document.getElementById('social').innerHTML='You already voted for that track!';}else if(r.indexOf('Your vote has been refused')!=-1){document.getElementById('social').innerHTML='Your vote has been refused.';}else{document.getElementById('social').innerHTML='Voted down.';}}};h.send(null);"
-thumbsDown.attr("href", tdAttr);
-thumbsDown.attr("onClicked", tdAttr);
-var upUrl = thumbsUp.attr("href");
-var tuAttr = "javascript:var h=new XMLHttpRequest();h.open('GET', '" + upUrl + "', true);h.onreadystatechange=function(){if(h.readyState==4&&h.status==200){document.getElementById('social').innerHTML='Voted up.';}};h.send(null);"
-thumbsUp.attr("href", tuAttr);
-thumbsUp.attr("onClicked", tuAttr);
+if (document.URL.match(/tracks\/\d+/) || document.URL.match(/tracks\/random/)) { // we're playing something
+	var both = $('h1 a[href^="tracks/"]');
+	var thumbsDown = both.first();
+	var thumbsUp = both.last();
+
+	var downUrl = thumbsDown.attr("href");
+	thumbsDown.attr("href", "javascript:vote('" + downUrl + "', 'Voted down.');");
+	var upUrl = thumbsUp.attr("href");
+	thumbsUp.attr("href", "javascript:vote('" + upUrl + "', 'Voted up.');");
+}
+
+var navMenu = $("#menu");
+var randomTrack = $("a[href^='tracks/random']");
+randomTrack.attr("href", "javascript:showRandomMenu();");
